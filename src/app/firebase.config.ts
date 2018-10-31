9c5bd4ee1837d56780c06f4c1987e3477838d456
igenil <43435719+igenil@users.noreply.github.com>
2018-10-31 12:17:46 +0100

    firebaseconfig

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+	fire: {
+        apiKey: "AIzaSyAfYBU0sojw6WmDmjPvx3GrFSnKg6OiVjw",
+        authDomain: "login-205b5.firebaseapp.com",
+        databaseURL: "https://login-205b5.firebaseio.com",
+        projectId: "login-205b5",
+        storageBucket: "login-205b5.appspot.com",
+        messagingSenderId: "994457813758"
+	}
+};
