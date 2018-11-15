d4f1f6186db4f20561d732ffaad98bea444c8e70
igenil <43435719+igenil@users.noreply.github.com>
2018-11-15 11:39:48 +0100

    editar jugador en proceso

---
+++ src/pages/editjugadormodal/editjugadormodal.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditjugadormodalPage } from './editjugadormodal';
+
+@NgModule({
+  declarations: [
+    EditjugadormodalPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EditjugadormodalPage),
+  ],
+})
+export class EditjugadormodalPageModule {}
--- src/providers/listajugadores/listajugadores.ts
+++ src/providers/listajugadores/listajugadores.ts
@@ -1,9 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { JUGADORES } from '../../data/data.jugadores';
-import { Jugador } from '../../interface/jugador.interfaces';
 import { EQUIPOS } from '../../data/data.equipos';
-import { Equipo } from '../../interface/equipo.interfaces';
 @Injectable()
 export class ListajugadoresProvider {
   jugadores=JUGADORES.slice(0);
