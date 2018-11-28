444d96047906fe43626482dac6df80e73d5b3e8f
igenil <43435719+igenil@users.noreply.github.com>
2018-11-28 09:20:56 +0100

    jornadas modal funcionando

--- src/pages/jornadasmodal/jornadasmodal.ts
+++ src/pages/jornadasmodal/jornadasmodal.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the JornadasmodalPage page.
@@ -14,12 +14,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'jornadasmodal.html',
 })
 export class JornadasmodalPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  jornada:any={};
+  constructor(public navCtrl: NavController, public navParams: NavParams,  public viewCtrl:ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad JornadasmodalPage');
+    let data=this.navParams.get('jornada');
+    this.jornada=data;
   }
 
 }
