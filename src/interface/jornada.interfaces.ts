db75793a6c258fee46ff06fdac69c1e6c82b0b00
JavierPartidaMolina <38098306+JavierPartidaMolina@users.noreply.github.com>
2018-11-28 10:43:25 +0100

    COSAS

--- src/interface/jornada.interfaces.ts
+++ src/interface/jornada.interfaces.ts
@@ -1,8 +1,12 @@
 export interface Jornada {
     nombre: string;
     fecha: string;
-    titular1: string;
-    titular2: string;
-    titular3: string;
-    titular4: string;
+    titular1: any;
+    titular2: any;
+    titular3: any;
+    titular4: any;
+    n_titular1: number;
+    n_titular2: number;
+    n_titular3: number;
+    n_titular4: number;
 }
