58c6c3b126e65efdb9db2b43507ae5407826037c
igenil <43435719+igenil@users.noreply.github.com>
2018-11-28 10:43:39 +0100

    this.jornada terminado

--- src/pages/calendariomodal/calendariomodal.ts
+++ src/pages/calendariomodal/calendariomodal.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { Jornada } from '../../interface/jornada.interfaces';
 import { ListajornadasProvider } from '../../providers/listajornadas/listajornadas';
+import { ListajugadoresProvider } from '../../providers/listajugadores/listajugadores';
 import * as moment from 'moment';
 
 /**
@@ -20,14 +21,25 @@ export class CalendariomodalPage {
   event = { startTime: new Date().toISOString(), endTime: new Date().toISOString(), Casa: false, Fuera: false, title:''}
   minDate = new Date().toISOString();
   jornada:Jornada;
-  constructor(public listajornadas:ListajornadasProvider,public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
+  titulares:any;
+  titular1:any;
+  titular2:any;
+  tirtular3:any;
+  titular4:any;
+  constructor(public listajornadas:ListajornadasProvider,public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController, public listajugadores:ListajugadoresProvider) {
     let preselectedDate = moment(this.navParams.get('selectedDay')).format();
     this.event.startTime = preselectedDate;
     this.event.endTime = preselectedDate;
     console.log( this.event.startTime)
   }
   save(){
-    this.jornada = {nombre: this.event.title, fecha: this.event.startTime,titular1:'paco',titular2:'javi',titular3:'nacho',titular4:'antonio'};
+    for (let index = 0; index < this.listajugadores.jugadores.length; index++) {
+      
+      if (this.listajugadores.jugadores[index].juega) {
+        this.titulares.push=this.listajugadores[index];
+      }
+    }
+    this.jornada = {nombre: this.event.title, fecha: this.event.startTime,titular1:this.titulares[0],titular2:this.titulares[1],titular3:this.titulares[2],titular4:this.titulares[3], n_titular1:0, n_titular2:0,n_titular3:0,n_titular4:0};
     this.listajornadas.jornadas.push(this.jornada);
     this.viewCtrl.dismiss(this.event)
   }
