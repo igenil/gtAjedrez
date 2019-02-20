52f318a9bee2ca18eb879978585a41ac492fe8e5
igenil <43435719+igenil@users.noreply.github.com>
2019-02-20 13:35:35 +0100

    capitanes

---
+++ src/pages/capitanes/capitanes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CapitanesPage } from './capitanes';
+
+@NgModule({
+  declarations: [
+    CapitanesPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CapitanesPage),
+  ],
+})
+export class CapitanesPageModule {}
---
+++ src/pages/capitanes/capitanes.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-capitanes',
+  templateUrl: 'capitanes.html',
+})
+export class CapitanesPage {
+  listEquipos: AngularFireList<any>;
+
+  constructor(public navCtrl: NavController, private afdb: AngularFireDatabase, private modalCtrl: ModalController, public navParams: NavParams) {
+    var capitanes = this.afdb.list('/jugador', ref => ref.orderByChild('capitan'));
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CapitanesPage');
+  }
+
+  mostrar_modal(){
+    this.listEquipos = this.afdb.list("/equipo");
+    let modal=this.modalCtrl.create(CapitanesmodalPage,{'listEquipos':this.listEquipos});
+    modal.present();
+  }
+}
