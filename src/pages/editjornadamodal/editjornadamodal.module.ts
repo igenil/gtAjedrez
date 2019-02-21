d370d36569a2c04a45555791db59a017b304f2ad
JavierPartidaMolina <38098306+JavierPartidaMolina@users.noreply.github.com>
2019-02-21 16:47:55 +0100

    edit Jornada Modal

---
+++ src/pages/editjornadamodal/editjornadamodal.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditjornadamodalPage } from './editjornadamodal';
+
+@NgModule({
+  declarations: [
+    EditjornadamodalPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EditjornadamodalPage),
+  ],
+})
+export class EditjornadamodalPageModule {}
---
+++ src/pages/editjornadamodal/editjornadamodal.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the EditjornadamodalPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-editjornadamodal',
+  templateUrl: 'editjornadamodal.html',
+})
+export class EditjornadamodalPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EditjornadamodalPage');
+  }
+
+}
